278dd033f6e2b360864440c75ae7028cb570c0e5
Sharjeel Baig <dr.sharjeel.6@gmail.com>
2025-11-20 17:25:53 +0500

    removed deepface

--- architecture/deepface_utils/recognition.py
+++
@@ -1,9 +0,0 @@
-from deepface import DeepFace
-def recognizeFace(image_url1: str, image_url2: str):
-    result = DeepFace.verify(image_url1, image_url2,
-                             detector_backend="retinaface",
-                             model_name="Facenet", threshold=0.5)
-    return {
-        "isMatch": result["verified"],
-        "confidence": result["distance"]
-    }
